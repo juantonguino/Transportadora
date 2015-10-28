45e02b72a8939bde71bc7fd24a78e66f8c8899ce
juantonguino <juan.tonguino@gmail.com>
2015-10-27 22:49:11 -0500

    se esta trabajando en transacciones

---
+++ src/java/com/beans/ServicioBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author juandiego
+ */
+@ManagedBean
+@RequestScoped
+public class ServicioBean {
+
+    /**
+     * Creates a new instance of ServicioBean
+     */
+    public ServicioBean() {
+    }
+    
+}
